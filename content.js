7e8c9a21ac585dcfadfd35b9071a60642ea479cb
TotalFitnessPiqua <gavril@totalfitnesspiqua.com>
2025-08-23 04:51:04 +0800

    bugfix

--- content.js
+++ content.js
@@ -1,14 +1,33 @@
 export const contentMap = {
   "day1-lesson1": {
     title: "Begin Eating A Balanced Healthy Diet",
-    video: "https://example.com/video1",
-    audio: "https://example.com/audio1",
-    document: "https://example.com/doc1"
+    video: "https://dl.dropboxusercontent.com/scl/fi/fvyrscjku7ijinow3ilfp/Day-1.mp4?rlkey=99j0eyuzfetzm7ktzd5g3vgmf&st=q2xaxmfp",
+    audio: "https://dl.dropboxusercontent.com/scl/fi/079k8bo3vc07cej5hki2q/Day-1-TTP-Audiobook.MP3?rlkey=da6cncn9xo9f2q86xqi2wab7d&st=uhru196x",
+    document: "https://dl.dropboxusercontent.com/scl/fi/f368cu4xffvwuzfrxklxe/Day-1-Begin-Eating-A-Balanced-Healthy-Diet.pdf?rlkey=5zoxxrv4lyzvx94o2ajojt8xu&st=8tq7rmyl"
   },
   "week1-lesson1": {
     title: "Basics of Exercise",
-    video: "https://example.com/video2",
+    video: "https://dl.dropboxusercontent.com/scl/fi/z1ks9ospumzqswp0bk5oc/Week-1-Basics-of-Exercise.mp4?rlkey=1p7l5a1bhol0gnfhngoh0w0zw&st=seqqwhtp",
+    audio: "https://example.com/audio1",
+    document: "https://dl.dropboxusercontent.com/scl/fi/0r33o902k6qslfslja4jz/Week-1-Basics-Of-Exercise.pdf?rlkey=k1squ9s73853s73xvglpe8hrf&st=wni0x67c"
+  },
+  "week1-lesson2": {
+    title: "Basics of Nutrition",
+    video: "https://dl.dropboxusercontent.com/scl/fi/cdzgxatytq6xxhs7j5lbp/Week-1-Basics-of-Nutrition.mp4?rlkey=jky6gjyqqot8da7i5jxmy4baf&st=w5rhfn0g",
     audio: "https://example.com/audio2",
-    document: "https://example.com/doc2"
-  }
+    document: "https://dl.dropboxusercontent.com/scl/fi/evg8uz0y4z0ab11ztj7xy/Week-1-Basics-of-Nutrition.pdf?rlkey=0x0afyw5nhgzkua7jfptu0ubl&st=tj5wluqz"
+  },
+  "week2-lesson1": {
+    title: "Aerobic Exercise",
+    video: "https://dl.dropboxusercontent.com/scl/fi/38itgscz63g41lhra3kbt/Week-2-Aerobic-Exercise.mp4?rlkey=uuzjypechormngc30j8h3q5ld&st=i25bxvuh",
+    audio: "https://example.com/audio3",
+    document: "https://www.dropbox.com/scl/fi/0owg1uhgva9m41lazp9rx/Week-2-Aerobic-Exercise.pdf?rlkey=wqfo5ah0g0ove2vm10od64sih&st=6fu7jqju&dl=0"
+  },
+  "week2-lesson2": {
+    title: "Carbohydrates",
+    video: "https://example.com/video4",
+    audio: "https://example.com/audio4",
+    document: "https://www.dropbox.com/scl/fi/p5sk2ihtez08ie5dkmciu/Week-2-Carbohydrates.pdf?rlkey=d4odmnrquvz1ecidthpw2z9nu&st=p77hrlkh&dl=0"
+  },
+  // ...continue adding more weeks/lessons
 };
